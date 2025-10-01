5996262a393acfe1e42fe30a46c36c0baf547ebc
Sheep-aron <kissing0927@gmail.com>
2025-10-01 08:44:07 +0800

    序列反序列准备就绪

--- demo/jsoncpp/jsoncpp.cc
+++ demo/jsoncpp/jsoncpp.cc
@@ -2,6 +2,7 @@
 #include <jsoncpp/json/json.h>
 #include <memory>
 #include <sstream>
+#include <string>
 
 int main()
 {
@@ -12,8 +13,8 @@ int main()
     stu["score"].append(97);
     stu["score"].append(98);
 
-    Json::StreamWriterBuilder builder;
-    std::unique_ptr<Json::StreamWriter> sw(builder.newStreamWriter());
+    Json::StreamWriterBuilder swb;
+    std::unique_ptr<Json::StreamWriter> sw(swb.newStreamWriter());
     std::stringstream ss;
     int ret = sw->write(stu, &ss);
     if (ret != 0)
@@ -22,5 +23,24 @@ int main()
         return -1;
     }
     std::cout << ss.str() << std::endl;
+
+    std::cout << "================================================" << std::endl;
+
+    std::string str = ss.str();
+    Json::Value root;
+    Json::CharReaderBuilder rb;
+    std::unique_ptr<Json::CharReader> cr(rb.newCharReader());
+    bool rat = cr->parse(str.c_str(), str.c_str() + str.size(), &root, nullptr);
+    if (!rat)
+    {
+        std::cerr << "parse json failed" << std::endl;
+        return -1;
+    }
+    std::cout << "age: " << root["age"].asInt() << "\nname: " << root["name"].asString();
+    std::cout << "\nscores: ";
+    for (const auto &s : root["score"])
+        std::cout << s.asInt() << " ";
+    std::cout << std::endl;
+
     return 0;
 } 
